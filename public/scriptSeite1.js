3507c2475c047f025c38412bacc73e8892eba14a
digilog11 <66838684+digilog11@users.noreply.github.com>
2020-06-12 21:07:14 +0200

    Create scriptSeite1.js

---
+++ public/scriptSeite1.js
@@ -0,0 +1,353 @@
+"use_strict";
+
+/**
+ * changes degrees into radians
+ * @param {number} degrees - the coordinate in degrees
+ * @return {number} - the coordinate in radians
+ */
+function toRadians (degrees) {
+  return degrees * (Math.PI/180);
+}
+
+/**
+ * changes radians into degrees
+ * @param {number} radians - the coordinate in radians
+ * @return {number} - the coordinate in degrees
+ */
+function toDegrees (radians) {
+  return radians * (180/Math.PI);
+}
+
+/**
+ * converts from unix time to human-readable time
+ * @param {number} unix - unix time in seconds
+ * @return {string} - time e.g. "10:04 Uhr"
+ */
+function toReadableTime (unix) {
+  var time = new Date (unix*1000);
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  if (minutes < 10){
+    return "" + hours + ":0" + minutes + " Uhr";
+  }else{
+    return "" + hours + ":" + minutes + " Uhr";
+  }
+}
+
+/**
+ * converts from seconds to minutes
+ * @param {number} seconds - time in seconds
+ * @return {string} - time in minutes e.g. "4 Minuten"
+ */
+function toMinutes (seconds) {
+  var minutes = Math.round(seconds/60);
+  if (minutes >= 1 && minutes < 2){
+    return "" + minutes + " Minute";
+  }else{
+    return "" + minutes + " Minuten";
+  }
+}
+
+/**
+ * rounds a number to three places behind the comma
+ * @param {number} num - input number
+ * @return {number} - rounded number
+ */
+function round3 (num) {
+  return (Math.round(num * (Math.pow(10,3)))) * Math.pow(10, -3);
+}
+
+/**
+ * calculates the distance between two points on the basis of a spherical earth
+ * @param {number} lon1 - Point 1 Longitude
+ * @param {number} lat1 - Point 1 Latitude
+ * @param {number} lon2 - Point 2 Longitude
+ * @param {number} lat2 - Point 2 Latitude
+ * @return {number} - the distance in kilometres
+ */
+function getDistance (lon1, lat1, lon2, lat2) {
+  var R = 6371e3; // metres
+  var φ1 = toRadians(lat1);
+  var φ2 = toRadians(lat2);
+  var Δφ = toRadians(lat2-lat1);
+  var Δλ = toRadians(lon2-lon1);
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d*0.001; // kilometres
+}
+
+/**
+ * calculates the bearing between two points on the basis of a spherical earth
+ * @param {number} lon1 - Point 1 Longitude
+ * @param {number} lat1 - Point 1 Latitude
+ * @param {number} lon2 - Point 2 Longitude
+ * @param {number} lat2 - Point 2 Latitude
+ * @return {number} - the bearing in degrees between 0 and 360
+ */
+function getBearing (lon1, lat1, lon2, lat2) {
+  var dlon = lon2-lon1;
+  var y = Math.sin(toRadians(dlon)) * Math.cos(toRadians(lat2));
+  var x = Math.cos(toRadians(lat1)) * Math.sin(toRadians(lat2)) -
+          Math.sin(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.cos(toRadians(dlon));
+  var brng0 = toDegrees(Math.atan2(y, x));
+  var brng1 = (brng0+360)%360;
+  if (brng1 < 0 || brng1 >= 360) {
+    throw "Error: Bearing outside 0-360°";
+  } else return brng1;
+}
+
+/**
+ * decides the compass direction relative from a point 1 to a point 2 using the getBearing function
+ * @param {number} lon1 - Point 1 Longitude
+ * @param {number} lat1 - Point 1 Latitude
+ * @param {number} lon2 - Point 2 Longitude
+ * @param {number} lat2 - Point 2 Latitude
+ * @return {string} - the direction as a string
+ */
+function getDirection (lon1, lat1, lon2, lat2) {
+  var brng = getBearing(lon1, lat1, lon2, lat2);
+  // the circle is divided into 8 segments of each 45°. N, NE, E, SE, S, SW, W and NW are each in the middle
+  // of the segments, i.e. N is at 0° and the North-Segment goes from -22.5°(or 337.5°) to 22.5°
+  if (brng <= 22.5 || brng > 337.5 ) return "N";
+  else if (brng > 22.5 && brng <= 67.5) return "NE";
+  else if (brng > 67.5 && brng <= 112.5) return "E";
+  else if (brng > 112.5 && brng <= 157.5) return "SE";
+  else if (brng > 157.5 && brng <= 202.5) return "S";
+  else if (brng > 202.5 && brng <= 247.5) return "SW";
+  else if (brng > 247.5 && brng <= 292.5) return "W";
+  else return "NW";
+}
+
+/**
+ * sorts the array in ascending order according to distance
+ * @param {object[]} array - input array of objects with property 'distance'
+ */
+function sortByDistance (array) {
+  array.sort(function (a, b) {return a.distance - b.distance});
+}
+
+/**
+ * sorts the array in ascending order according to time
+ * @param {object[]} array - input array of objects with property 'departure_time'
+ */
+function sortByTime (array) {
+  array.sort(function (a, b) {return a.departure_time - b.departure_time});
+}
+
+/**
+ * uses xhr to request a JSON from an url and after success invokes a function
+ * @param {string} url - url from which to request the JSON
+ * @param {string} cFunction - name of function to be called after request is ready
+ * @param {object} object - parameter of cFunction
+ */
+function requestJSON (url, cFunction, object) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+     if(request.readyState == 4 && request.status == "200"){
+       cFunction(this, object);
+     }
+   };
+   request.open("GET", url, true);
+   request.send();
+}
+
+/**
+ * uses xhr to request a JSON from an url and after success invokes a function
+ * same as function requestJSON, but doesn't give on an object
+ * @param {string} url - url from which to request the JSON
+ * @param {string} cFunction - name of function to be called after request is ready
+ */
+function requestJSON2 (url, cFunction) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+     if(request.readyState == 4 && request.status == "200"){
+       cFunction(this);
+     }
+   };
+   request.open("GET", url, true);
+   request.send();
+}
+
+/**
+ * @constructor creates a new Busstop
+ * @property {string} name - the name of the Busstop
+ * @property {number} number - the number of the Busstop
+ * @property {number} distance - the distance of the Busstop
+ * @property {string} direction - the direction of the Busstop
+ * @property {number[]} coordinates - the coordinates of the Busstop
+ * @example new Busstop = {name: "Domplatz", number: 345673, distance: 0.877, direction: "SW", coordinates: [51.96,7.63]}
+ */
+function Busstop (name, number, distance, direction, coordinates) {
+  this.name = name
+  this.number = number;
+  this.distance = distance;
+  this.direction = direction;
+  this.coordinates = coordinates;
+}
+
+/**
+ * @constructor creates a new Ride
+ * @property {number} departure_time - the time of departure in unix seconds
+ * @property {number} delay - the delay in seconds
+ * @property {string} line - the line number of the Ride
+ * @property {string} destination - the destination of the Ride
+ * @example new Ride = {departure_time: 1588851540, delay: 8, line: "9", destination: "Von-Humboldt-Straße"}
+ */
+function Ride (departure_time, delay, line, destination) {
+  this.departure_time = departure_time;
+  this.delay = delay;
+  this.line = line;
+  this.destination = destination;
+}
+
+/**
+ * uses the response from the requestJSON function to create an array of Busstop objects, sorts the array
+ * and displays it in the html doc, also requests the busrides at the nearest busstop in the next 20 minutes
+ * @param {object} request - XMLHttpRequest
+ * @param {array} point - used to calculate distance and direction between point and busstops
+ */
+function displayBusstops (request, point) {
+  var busstops = JSON.parse(request.response);
+  var sortedBusstops = new Array();
+
+  // fill the new array with Busstop objects
+  for (var i = 0; i < busstops.features.length; i++){
+    sortedBusstops[i] = new Busstop (
+      busstops.features[i].properties.lbez,
+      busstops.features[i].properties.nr,
+      round3(getDistance(point.lon,point.lat,busstops.features[i].geometry.coordinates[0],busstops.features[i].geometry.coordinates[1])),
+      getDirection(point.lon,point.lat,busstops.features[i].geometry.coordinates[0],busstops.features[i].geometry.coordinates[1]),
+      [busstops.features[i].geometry.coordinates[0],busstops.features[i].geometry.coordinates[1]]
+    )
+  }
+
+  sortByDistance(sortedBusstops);
+
+  // get Busrides at nearest busstop
+  // sekunden = 1200 => 20 minutes
+  requestJSON('https://rest.busradar.conterra.de/prod/haltestellen/' + sortedBusstops[0].number
+   + '/abfahrten?sekunden=1200', displayNearestBusstop, sortedBusstops);
+
+  // write the sorted array into the html doc
+  for (var i = 1; i < 5; i++){
+     document.getElementById("busstops").innerHTML +=
+       "<p>" + "<b>" + "Bushaltestelle: " + "</b>" + sortedBusstops[i].name + "<br>" +
+       "<b>" + "Distanz zum Standort: " + "</b>" + sortedBusstops[i].distance + " km" + "<br>" +
+       "<b>" + "Himmelsrichtung: " + "</b>" + sortedBusstops[i].direction + "</p>" ;
+  }
+
+  // create a Leaflet map, center is position of user
+  var map = L.map('mapId').setView([point.lat,point.lon],15);
+     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+       maxZoom:18,
+       attribution: 'Leaflet, OpenStreetMap Contributors',
+   }).addTo(map);
+
+  // add a marker at the position of user
+  L.marker([point.lat,point.lon]).addTo(map).bindPopup("Dein Standort").openPopup();
+
+  // add markers for the busstops and popups with information about name, distance and
+  // direction of the busstop
+  for (var i = 0; i < sortedBusstops.length; i++){
+    L.marker([sortedBusstops[i].coordinates[1],sortedBusstops[i].coordinates[0]]).addTo(map).bindPopup(
+       "<p>" + "<b>" + "Bushaltestelle: " + "</b>" + sortedBusstops[i].name + "<br>" +
+       "<b>" + "Distanz zum Standort: " + "</b>" + sortedBusstops[i].distance + " km" + "<br>" +
+       "<b>" + "Himmelsrichtung: " + "</b>" + sortedBusstops[i].direction + "</p>" );
+ }
+}
+
+/**
+ * uses the response from the requestJSON function to create an array of Ride objects, sorts the array
+ * and displays it in the html doc after also displaying the nearest busstop
+ * @param {object} request - XMLHttpRequest
+ * @param {object} sortedBusstops - used to display the nearest busstop
+ */
+function displayNearestBusstop (request, sortedBusstops) {
+  var rides = JSON.parse(request.response);
+  var sortedRides = new Array();
+
+  // fill the new array with Ride objects
+  for (var i = 0; i < rides.length; i++){
+    sortedRides[i] = new Ride (
+      rides[i].abfahrtszeit,
+      rides[i].delay,
+      rides[i].linientext,
+      rides[i].richtungstext
+    )
+  }
+
+  sortByTime(sortedRides);
+
+  // write explanation of colour-coding
+  document.getElementById("info").innerHTML =
+    "Bushaltestellen werden in schwarz angezeigt, Fahrten in blau";
+
+  // write the nearest busstop into the html doc
+  document.getElementById("nearest_busstop").innerHTML +=
+    "<p>" + "<b>" + "Bushaltestelle: " + "</b>" + sortedBusstops[0].name + "<br>" +
+    "<b>" + "Distanz: " + "</b>" + sortedBusstops[0].distance + " km" + "<br>" +
+    "<b>" + "Himmelsrichtung: " + "</b>" + sortedBusstops[0].direction + "</p>";
+
+  // write the busrides at the nearest busstop into the html doc
+  if (rides.length == 0) {
+    document.getElementById("nearest_busstop").innerHTML += "<p style='color:blue;'>" + "Keine Fahrten in den nächsten 5 Minuten" + "</p>";
+  }else{
+    for (var i = 0; i < sortedRides.length; i++){
+      document.getElementById("nearest_busstop").innerHTML +=
+      "<p style='color:blue;'>" + "<b>" + "Abfahrtszeit: " + "</b>" + toReadableTime(sortedRides[i].departure_time) + "<br>" +
+      "<b>" + "Verspätung: " + "</b>" + toMinutes(sortedRides[i].delay) + "<br>" +
+      "<b>" + "Linie: " + "</b>" + sortedRides[i].line + "<br>" +
+      "<b>" + "Richtung: " + "</b>" + sortedRides[i].destination + "</p>";
+    }
+  }
+}
+
+/**
+ * requests the busstops from the conterra API, gives selected point on to function displayBusstops()
+ * @param {number} lon - longitude
+ * @param {number} lat - latitude
+ */
+ function calculateBusDistance (lon, lat) {
+   // clear area
+   document.getElementById("busstops").innerHTML = "";
+   document.getElementById("nearest_busstop").innerHTML = "";
+
+   var x = {lon: lon, lat: lat};
+
+   // get busstops
+   requestJSON('https://rest.busradar.conterra.de/prod/haltestellen', displayBusstops, x);
+ }
+
+ /**
+  * gets the selected point and gives the lon and lat values of that point on to function calculateBusDistance()
+  */
+ function selectPoint() {
+   var radioButtons = document.getElementsByName("point");
+
+   for (var i = 0; i < radioButtons.length; i++){
+     if(radioButtons[i].checked == true){
+       calculateBusDistance(radioButtons[i].value, radioButtons[i].id);
+     }
+   }
+ }
+
+ /**
+  * uses the response from the requestJSON function to display the points from the MongoDB database
+  * in a form as selectable radio buttons
+  * @param {object} request - XMLHttpRequest
+  */
+ function loadPoints (request) {
+   var points = JSON.parse(request.response);
+
+   // for each point create a radio button
+   for(var i = 0; i < points.length; i++){
+     document.getElementById("radioSelectPoint").innerHTML +=
+       "<input type = 'radio' name = 'point' value =" + points[i].lon + " id =" + points[i].lat + ">" +
+       "<label for = " + points[i]._id + ">" + "lon: " + points[i].lon + ", lat: " + points[i].lat + " </label><br>";
+   }
+ }
+
+requestJSON2("http://localhost:3000/points", loadPoints);
